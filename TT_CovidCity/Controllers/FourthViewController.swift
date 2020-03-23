2198a6d9b50223f97f71203a8633141739db1925
Tran Nhat Thong <yunotran123@gmail.com>
2020-03-23 13:45:39 +0700

    Finish Tab Bar

---
+++ TT_CovidCity/Controllers/FourthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FourViewController.swift
+//  abc
+//
+//  Created by Trần Nhất Thống on 3/23/20.
+//  Copyright © 2020 Trần Nhất Thống. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tabBarItem.selectedImage = UIImage.init(systemName: "paperplane.fill")
+  }
+  
+  
+}
